a79cd82a4f9799181b80bdbac567d5baa5868a41
Amarjeet Singh <amarjeetsingh1999@ymail.com>
2020-10-29 12:10:06 +0530

    Updates

---
+++ precache-manifest.5c5bf2c7ce11565ba7227bd3b3ee5ce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb547f60cc578464b954652684adfdcb",
+    "url": "/Events-Scheduler/index.html"
+  },
+  {
+    "revision": "46918628856bad1e6d20",
+    "url": "/Events-Scheduler/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0b0bbff04c03553d6388",
+    "url": "/Events-Scheduler/static/css/main.a39c187e.chunk.css"
+  },
+  {
+    "revision": "46918628856bad1e6d20",
+    "url": "/Events-Scheduler/static/js/2.93c9aab1.chunk.js"
+  },
+  {
+    "revision": "220ce693b6dfa473e6e0156ac2ec8d5d",
+    "url": "/Events-Scheduler/static/js/2.93c9aab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0bbff04c03553d6388",
+    "url": "/Events-Scheduler/static/js/main.21a6d43a.chunk.js"
+  },
+  {
+    "revision": "171582b04ffae254238d",
+    "url": "/Events-Scheduler/static/js/runtime-main.c1175a21.js"
+  }
+]);
